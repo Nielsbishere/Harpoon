198a928af5fd0bee681925db6f5ed0ee3650abbd
BLC\Niels <n@osomi.net>
2018-10-05 16:10:30 +0200

    [Harpoon] Error logging for HResult. Changed to reflective DLL injection instead of regular. Changed command line syntax to exclude .dll for injected dll name.

--- Harpoon/src/harpoon.cpp
+++ Harpoon/src/harpoon.cpp
@@ -6,6 +6,8 @@
 #include <process.h>
 #include <fstream>
 #include <psapi.h>
+#include <vector>
+#include <unordered_map>
 #pragma comment(lib, "Psapi.lib")
 using namespace hp;
 
@@ -13,7 +15,24 @@ template<typename T>
 void check(T func, char *str) {
 
 	if (func == 0) {
-		printf("%s\n", str);
+
+		HRESULT last = GetLastError();
+		printf("%s", str);
+
+		LPTSTR errorText = NULL;
+
+		FormatMessage(
+			FORMAT_MESSAGE_FROM_SYSTEM
+			| FORMAT_MESSAGE_ALLOCATE_BUFFER
+			| FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL, last, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&errorText,  0, NULL);
+
+		if (errorText != NULL) {
+			printf(": %s", errorText);
+			LocalFree(errorText);
+		} else printf("\n");
+
 		assert(false && "Error thrown");
 	}
 }
@@ -56,86 +75,193 @@ DWORD findProcess(std::string str) {
 	return 0;
 }
 
-LPVOID allocString(HANDLE process, std::string str) {
+LPVOID allocBuffer(HANDLE process, void *start, DWORD length, bool exec) {
 
-	DWORD size = (DWORD) str.size() + 1;
-	LPVOID remote = VirtualAllocEx(process, NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	LPVOID remote = VirtualAllocEx(process, NULL, length, MEM_COMMIT | MEM_RESERVE, exec ? PAGE_EXECUTE_READWRITE : PAGE_READWRITE);
 	check(remote, "Couldn't allocate space into process");
-	check(WriteProcessMemory(process, remote, (PVOID) str.c_str(), size, NULL), "Couldn't write string into memory");
+	check(WriteProcessMemory(process, remote, (LPVOID)start, length, NULL), "Couldn't write buffer into memory");
 
 	return remote;
 }
 
-void Harpoon::hook(DWORD processId, std::string dllPath) 
-{
-	std::string dllName = dllPath.substr(dllPath.find_last_of('\\') + 1);
-	std::string dllBase = dllPath.substr(0, dllPath.find_last_of('\\'));
+struct CopyDll {
 
-	//Figure out where the initialize function is located in the dll
-	//So we can run it later
+	HINSTANCE current;
+	size_t length;
 
-	HMODULE loc = LoadLibraryA(dllPath.c_str());
-	printf("Checking for library at path %s\n", dllPath.c_str());
+	PVOID remote;
+
+};
+
+CopyDll loadCopyDll(const char *path, HANDLE process) {
+
+	//Load library into our memory
+	HINSTANCE loc = LoadLibraryA(path);
+	printf("Checking for library at path %s\n", path);
 	check(loc, "Can't load dll");
 
-	FARPROC dllFunc = GetProcAddress(loc, "initialize");
-	check(dllFunc, "Can't load dll function");
+	//Get our dll's module info
+	MODULEINFO info;
+	check(GetModuleInformation(GetCurrentProcess(), loc, &info, (DWORD) sizeof(info)), "Couldn't get module info");
 
-	size_t dllFuncOff = (size_t)dllFunc - (size_t)loc;
-	printf("%s::initialize found at %p\n", dllPath.c_str(), (void*) dllFuncOff);
+	return { loc, (size_t)info.SizeOfImage, allocBuffer(process, loc, info.SizeOfImage, true) };
+}
+
+PVOID getRemoteFunc(CopyDll dll, const char *func) {
+
+	//Get the function in our dll
+	FARPROC proc = GetProcAddress(dll.current, func);
+	check(proc, "Can't load dll function");
+
+	//Get dif to current dll
+	size_t dif = size_t((char*)proc - (char*)dll.current);
+
+	return (char*)dll.remote + dif;
+
+}
+
+struct RemoteDll {
+	std::string name, fileName;
+	HINSTANCE remote;
+};
+
+RemoteDll getRemoteDll(HANDLE process, std::string module) {
+
+	HINSTANCE handles[2048];
+	DWORD needed = 0;
+	EnumProcessModulesEx(process, handles, sizeof(handles), &needed, LIST_MODULES_ALL);
+
+	char name[MAX_PATH + 1], fileName[MAX_PATH + 1];
+
+	for (int i = 0; i < needed / sizeof(handles[0]); ++i) {
+
+		GetModuleBaseNameA(process, handles[i], name, sizeof(name));
+		GetModuleFileNameExA(process, handles[i], fileName, sizeof(fileName));
+
+		MODULEINFO modInfo;
+		GetModuleInformation(process, handles[i], &modInfo, sizeof(modInfo));
+
+		if (module == name)
+			return { name, fileName, handles[i] };
+
+	}
+
+	check(false, "Couldn't find specified module");
+	return {};
+}
+
+ 
+#ifdef _WIN64
+#define padptr "\0\0\0\0\0\0\0\0"
+#else
+#define padptr "\0\0\0\0"
+#endif
+
+std::unordered_map<std::string, void*> getFunctions(RemoteDll dll, std::vector<std::string> functions) {
+
+	HINSTANCE local = LoadLibraryExA(dll.fileName.c_str(), NULL, DONT_RESOLVE_DLL_REFERENCES);
+	if (local == NULL) {
+		printf("Couldn't load remote library\n");
+		return {};
+	}
+
+	std::unordered_map<std::string, void*> funcs;
+
+	for (std::string &str : functions)
+		funcs[str] = (void*)((char*)GetProcAddress(local, str.c_str()) - (char*)local + (char*)dll.remote);
+
+	FreeLibrary(local);
+	return funcs;
+
+}
+
+void Harpoon::hook(DWORD processId, std::string dllPath) {
 
 	//Get process
+
 	HANDLE process = OpenProcess(
-		PROCESS_QUERY_INFORMATION | PROCESS_CREATE_THREAD | 
+		PROCESS_QUERY_INFORMATION | PROCESS_CREATE_THREAD |
 		PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ,
 		FALSE, processId
 	);
 
 	check(process, "Couldn't open process");
 
-	//Copy the string into the other process
-	LPVOID dllStr = allocString(process, dllPath);
+	/*IMAGE_NT_HEADERS *headers = ImageNtHeader(process);
 
-	//Load the dll into memory
-	PTHREAD_START_ROUTINE loadLibraryProc = (PTHREAD_START_ROUTINE) GetProcAddress(GetModuleHandle(TEXT("kernel32")), "LoadLibraryA");
-	check(loadLibraryProc, "Couldn't get 'load library'");
+	bool is64bit = headers->FileHeader.Machine == IMAGE_FILE_MACHINE_AMD64;
 
-	//Run the load library function
-	HANDLE thread = CreateRemoteThread(process, NULL, 0, loadLibraryProc, dllStr, 0, NULL);
+	#ifdef _WIN64
+	check(is64bit, "Couldn't inject into a 32-bit process from a 64-bit harpoon");
+	#else
+	check(!is64bit, "Couldn't inject into a 64-bit process from a 32-bit harpoon");
+	#endif*/
+
+	//TODO: check is64bit
+
+	//Load our dll into the host's process and mono into our process
+	CopyDll harpoon = loadCopyDll(dllPath.c_str(), process);
+	RemoteDll mono = getRemoteDll(process, "mono.dll");
+
+	//Setup a command to get our functions from DLL
+	std::vector<std::string> funcs = {
+		"mono_domain_get",
+		"mono_thread_attach",
+		"mono_get_root_domain",
+		"mono_domain_assembly_open",
+		"mono_class_from_name",
+		"mono_class_get_method_from_name",
+		"mono_runtime_invoke",
+		"mono_assembly_get_image"
+	};
+
+	auto functionMap = getFunctions(mono, funcs);
+
+	//These are our strings (and functions); but we can't use those directly
+	//This is because this program will be moved and any address (not to kernel32) will have to be moved
+	char data[] = {
+		"Harpoon.Core\0"						//0
+		"HarpoonCore\0"							//13
+		"Initialize\0"							//25
+		"Harpoon.Core.dll\0"					//36
+		padptr									//53;	fmono_domain_get
+		padptr									//		fmono_thread_attach
+		padptr									//		fmono_get_root_domain
+		padptr									//		fmono_domain_assembly_open
+		padptr									//		fmono_class_from_name
+		padptr									//		fmono_class_get_method_from_name
+		padptr									//		fmono_runtime_invoke
+		padptr									//		fmono_assembly_get_image
+	};
+
+	constexpr size_t pad = sizeof(void*);
+
+	*(void**)(data + 53) = functionMap["mono_domain_get"];
+	*(void**)(data + 53 + pad) = functionMap["mono_thread_attach"];
+	*(void**)(data + 53 + pad * 2) = functionMap["mono_get_root_domain"];
+	*(void**)(data + 53 + pad * 3) = functionMap["mono_domain_assembly_open"];
+	*(void**)(data + 53 + pad * 4) = functionMap["mono_class_from_name"];
+	*(void**)(data + 53 + pad * 5) = functionMap["mono_class_get_method_from_name"];
+	*(void**)(data + 53 + pad * 6) = functionMap["mono_runtime_invoke"];
+	*(void**)(data + 53 + pad * 7) = functionMap["mono_assembly_get_image"];;
+
+	LPVOID dat = allocBuffer(process, data, (DWORD) sizeof(data), false);
+
+	//Run our remote function
+	PTHREAD_START_ROUTINE initialize = (PTHREAD_START_ROUTINE) getRemoteFunc(harpoon, "initialize");
+	printf("Running %s::initialize at %p\n", dllPath.c_str(), (void*)initialize);
+
+	HANDLE thread = CreateRemoteThread(process, NULL, 0, initialize, dat, 0, NULL);
 	check(thread, "Couldn't start library on remote process");
 
 	//Wait for it to finish and stop
 	WaitForSingleObject(thread, INFINITE);
 
-	//Check if it was successfully injected
-
-	HMODULE handles[2048];
-	DWORD needed = 0;
-	EnumProcessModules(process, handles, sizeof(handles), &needed);
-
-	char name[1024];
-
-	for (int i = 0; i < needed / sizeof(handles[0]); ++i) {
-
-		GetModuleBaseName(process, handles[i], name, sizeof(name));
-
-		if (std::string(name) == dllName) {
-
-			PTHREAD_START_ROUTINE remoteInitialize = (PTHREAD_START_ROUTINE)(((char*) handles[i]) + dllFuncOff);
-
-			printf("Found injected DLL in process (%s with address %p)\n", name, (void*) handles[i]);
-			printf("Running initialize at %p\n", remoteInitialize);
-
-			HANDLE dllThread = CreateRemoteThread(process, NULL, 0, remoteInitialize, NULL, 0, NULL);
-			check(dllThread, "Couldn't run initialize on remote thread");
-
-			WaitForSingleObject(dllThread, INFINITE);
-			CloseHandle(dllThread);
-		}
-	}
-
-	VirtualFreeEx(process, dllStr, 0, MEM_RELEASE);
+	VirtualFreeEx(process, dat, 0, MEM_RELEASE);
+	VirtualFreeEx(process, harpoon.remote, 0, MEM_RELEASE);
 	CloseHandle(thread);
+	FreeLibrary(harpoon.current);
+
 	CloseHandle(process);
 
 }
@@ -174,7 +300,7 @@ int main(int argc, char *argv[]) {
 		if (argc < 4) return help("Syntax: -hookid <pId> <dllPath>", false);
 
 		std::string pid = argv[2];
-		std::string dll = fullPath + "\\" + argv[3];
+		std::string dll = fullPath + "\\" + argv[3] + ".dll";
 
 		int id = std::stoi(pid);
 
@@ -182,12 +308,13 @@ int main(int argc, char *argv[]) {
 
 		Harpoon::hook((DWORD)id, dll);
 		return 1;
-	} else if (arg == "-hook") {
+	}
+	else if (arg == "-hook") {
 
 		if (argc < 4) return help("Syntax: -hook <pName> <dllPath>", false);
 
 		std::string pid = argv[2];
-		std::string dll = fullPath + "\\" + argv[3];
+		std::string dll = fullPath + "\\" + argv[3] + ".dll";
 
 		DWORD id = findProcess(pid);
 
---
+++ HarpoonLoader/harpoonloader.c
@@ -0,0 +1,56 @@
+
+#ifdef _WIN64
+#define padptr 8
+#else
+#define padptr 4
+#endif
+
+#define sHarpoon_Core strings + 0
+#define sHarpoonCore strings + 13
+#define sInitialize strings + 25
+#define sHarpoon_Core_dll strings + 36
+#define fmono_domain_get strings + 53
+#define fmono_thread_attach strings + 53 + padptr
+#define fmono_get_root_domain strings + 53 + padptr * 2
+#define fmono_domain_assembly_open strings + 53 + padptr * 3
+#define fmono_class_from_name strings + 53 + padptr * 4
+#define fmono_class_get_method_from_name strings + 53 + padptr * 5
+#define fmono_runtime_invoke strings + 53 + padptr * 6
+#define fmono_assembly_get_image strings + 53 + padptr * 7
+
+//Minimal mono injector
+__declspec(dllexport) unsigned int initialize(char *strings) {
+
+	//Attach to root domain
+
+	void* (*mono_thread_attach)(void*) = *(void**)(fmono_thread_attach);
+	void* (*mono_get_root_domain)() = *(void**)(fmono_get_root_domain);
+
+	mono_thread_attach(mono_get_root_domain());
+
+	//Get domain
+
+	void* (*mono_domain_get)() = *(void**)(fmono_domain_get);
+	void* (*mono_domain_assembly_open)(void*, char*) = *(void**)(fmono_domain_assembly_open);
+
+	void* domainAssembly = mono_domain_assembly_open(mono_domain_get(), sHarpoon_Core_dll);
+
+	//Get function in class
+
+	void* (*mono_class_from_name)(void*, char*, char*) = *(void**)(fmono_class_from_name);
+	void* (*mono_class_get_method_from_name)(void*, char*, unsigned int) = *(void**)(fmono_class_get_method_from_name);
+	void* (*mono_assembly_get_image)(void*) = *(void**)(fmono_assembly_get_image);
+
+	void *image = mono_assembly_get_image(domainAssembly);
+	void *monoClass = mono_class_from_name(image, sHarpoon_Core, sHarpoonCore);
+	void *monoMethod = mono_class_get_method_from_name(monoClass, sInitialize, 0);
+
+	//Invoke function
+
+	void* (*mono_runtime_invoke)(void*, void*, void**, void*) = *(void**)(fmono_runtime_invoke);
+
+	mono_runtime_invoke(monoMethod, 0, 0, 0);
+
+	return 1;
+
+}
